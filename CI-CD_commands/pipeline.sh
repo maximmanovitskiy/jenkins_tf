e5cbe0a15f9bd14843b6e0bfc67825d4fcc0982e
Maxim Manovitskiy <max@max.com>
2021-03-29 11:48:20 +0300

    pipeline

--- CI-CD_commands/pipeline.sh
+++ CI-CD_commands/pipeline.sh
@@ -4,15 +4,21 @@ pipeline {
         stage('Modify yml') {
             steps {
               sh '''
-                wget https://raw.githubusercontent.com/gitmaks/jenkins_tf/main/k8s_files/namespace.yml
-                wget https://raw.githubusercontent.com/gitmaks/jenkins_tf/main/k8s_files/deploy.yml
-                wget https://raw.githubusercontent.com/gitmaks/jenkins_tf/main/k8s_files/service.yml
-                wget https://raw.githubusercontent.com/gitmaks/jenkins_tf/main/k8s_files/ingress.yml
-                wget https://raw.githubusercontent.com/gitmaks/jenkins_tf/main/k8s_files/ingress-controller.yaml
-                sed -i "s/IMAGE_TAG/$IMAGE_TAG/g" *.yml
-                sed -i "s/ENV/$ENV/g" *.yml
-                sed -i "s/ACCOUNT_ID/${ACCOUNT_ID}/g" *.yml
-                sed -i "s/AWS_REGION/${AWS_REGION}/g" *.yml
+                mkdir "$ENV-${BUILD_NUMBER}"
+                wget -O $ENV-${BUILD_NUMBER}/namespace.yml \
+                https://raw.githubusercontent.com/gitmaks/jenkins_tf/main/k8s_files/namespace.yml
+                wget -O $ENV-${BUILD_NUMBER}/deploy.yml \
+                https://raw.githubusercontent.com/gitmaks/jenkins_tf/main/k8s_files/deploy.yml
+                wget -O $ENV-${BUILD_NUMBER}/service.yml \
+                https://raw.githubusercontent.com/gitmaks/jenkins_tf/main/k8s_files/service.yml
+                wget -O $ENV-${BUILD_NUMBER}/ingress.yml \
+                https://raw.githubusercontent.com/gitmaks/jenkins_tf/main/k8s_files/ingress.yml
+                wget -O $ENV-${BUILD_NUMBER}/ingress-controller.yaml \
+                https://raw.githubusercontent.com/gitmaks/jenkins_tf/main/k8s_files/ingress-controller.yaml
+                sed -i "s/IMAGE_TAG/$IMAGE_TAG/g" $ENV-${BUILD_NUMBER}/*.yml
+                sed -i "s/ENV/$ENV/g" $ENV-${BUILD_NUMBER}/*.yml
+                sed -i "s/ACCOUNT_ID/${ACCOUNT_ID}/g" $ENV-${BUILD_NUMBER}/*.yml
+                sed -i "s/AWS_REGION/${AWS_REGION}/g" $ENV-${BUILD_NUMBER}/*.yml
               '''
             }
         }
@@ -20,10 +26,10 @@ pipeline {
            steps {
              sh '''
                aws eks update-kubeconfig --name nginx-eks
-               kubectl apply -f namespace.yml
-               kubectl apply -f deploy.yml
-               kubectl apply -f service.yml
-               kubectl apply -f ingress.yml
+               kubectl apply -f $ENV-${BUILD_NUMBER}/namespace.yml
+               kubectl apply -f $ENV-${BUILD_NUMBER}/deploy.yml
+               kubectl apply -f $ENV-${BUILD_NUMBER}/service.yml
+               kubectl apply -f $ENV-${BUILD_NUMBER}/ingress.yml
              '''
            }
          }
@@ -54,8 +60,8 @@ pipeline {
             steps {
               sh '''
                kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.0.2/cert-manager.yaml
-               sed -i "s/CLUSTER_NAME/${CLUSTER_NAME}/g" ingress-controller.yaml
-               kubectl apply -f ingress-controller.yaml
+               sed -i "s/CLUSTER_NAME/${CLUSTER_NAME}/g" $ENV-${BUILD_NUMBER}/ingress-controller.yaml
+               kubectl apply -f $ENV-${BUILD_NUMBER}/ingress-controller.yaml
              '''
            }
          }
