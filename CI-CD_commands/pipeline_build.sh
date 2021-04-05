2c82f064898fe0a252e3b8c8f4c5ce8ddb29e263
max <max@max.com>
2021-04-05 11:15:59 +0300

    test

--- CI-CD_commands/pipeline_build.sh
+++ CI-CD_commands/pipeline_build.sh
@@ -34,7 +34,7 @@ pipeline {
                sudo docker run -d -p 1234:80 nginx_test:${BUILD_NUMBER}
 	       sleep 3
 	       curl localhost:1234 || sudo docker stop "$(sudo docker ps -q)"
-	       sudo docker exec -it "$(sudo docker ps -q)" nginx -v || sudo docker stop "$(sudo docker ps -q)"
+	       sudo docker exec  "$(sudo docker ps -q)" nginx -v || sudo docker stop "$(sudo docker ps -q)"
 	       sudo docker stop "$(sudo docker ps -q)" && echo Success
              '''
            }
